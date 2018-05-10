229195d55c833a3efa45896e5d193de120845a4b
saranya2611 <saranyahighflier@gmail.com>
2018-05-10 19:13:09 +0530

    Changed analyzeAllotmentsWizard4 outline of 2nd analysis window

--- src/gui/analyzeAllotmentsWizard4.java
+++ src/gui/analyzeAllotmentsWizard4.java
@@ -179,28 +179,22 @@ public class analyzeAllotmentsWizard4 extends JFrame {
      */
     private void $$$setupUI$$$() {
         jPanelAnalyseWizard4 = new JPanel();
-        jPanelAnalyseWizard4.setLayout(new FormLayout("fill:d:grow,left:4dlu:noGrow,fill:422px:noGrow,left:8dlu:noGrow,fill:71px:noGrow,left:4dlu:noGrow,fill:165px:noGrow,left:4dlu:noGrow,fill:19px:noGrow", "center:d:noGrow,top:16dlu:noGrow,center:max(d;4px):noGrow,top:17dlu:noGrow,center:max(d;4px):noGrow,top:21dlu:noGrow,center:max(d;4px):noGrow,top:23dlu:noGrow,center:max(d;4px):noGrow,top:22dlu:noGrow,center:max(d;4px):noGrow,top:16dlu:noGrow,center:37px:noGrow"));
+        jPanelAnalyseWizard4.setLayout(new FormLayout("fill:285px:grow,left:9dlu:noGrow,fill:452px:noGrow,left:9dlu:noGrow,fill:31px:noGrow,left:8dlu:noGrow,fill:143px:noGrow,left:4dlu:noGrow,fill:19px:noGrow", "center:12px:noGrow,top:4dlu:noGrow,center:d:noGrow,top:16dlu:noGrow,center:max(d;4px):noGrow,top:17dlu:noGrow,center:max(d;4px):noGrow,top:21dlu:noGrow,center:max(d;4px):noGrow,top:23dlu:noGrow,center:max(d;4px):noGrow,top:22dlu:noGrow,center:max(d;4px):noGrow,top:16dlu:noGrow,center:37px:noGrow"));
         jPanelAnalyseWizard4.setBackground(new Color(-14793908));
-        titleFieldAnalysisWizard4 = new JLabel();
-        Font titleFieldAnalysisWizard4Font = this.$$$getFont$$$("Lobster Two", Font.BOLD, 60, titleFieldAnalysisWizard4.getFont());
-        if (titleFieldAnalysisWizard4Font != null) titleFieldAnalysisWizard4.setFont(titleFieldAnalysisWizard4Font);
-        titleFieldAnalysisWizard4.setForeground(new Color(-301758));
-        titleFieldAnalysisWizard4.setText("Welcome to SEAT");
-        CellConstraints cc = new CellConstraints();
-        jPanelAnalyseWizard4.add(titleFieldAnalysisWizard4, cc.xyw(1, 1, 8, CellConstraints.CENTER, CellConstraints.DEFAULT));
         jLabelAnalysisWizard4 = new JLabel();
         Font jLabelAnalysisWizard4Font = this.$$$getFont$$$("Gentium Book Basic", Font.BOLD, 32, jLabelAnalysisWizard4.getFont());
         if (jLabelAnalysisWizard4Font != null) jLabelAnalysisWizard4.setFont(jLabelAnalysisWizard4Font);
         jLabelAnalysisWizard4.setForeground(new Color(-1774964));
         jLabelAnalysisWizard4.setText("Allotment Analysis ....");
-        jPanelAnalyseWizard4.add(jLabelAnalysisWizard4, cc.xyw(1, 3, 8, CellConstraints.CENTER, CellConstraints.FILL));
+        CellConstraints cc = new CellConstraints();
+        jPanelAnalyseWizard4.add(jLabelAnalysisWizard4, cc.xyw(3, 5, 2, CellConstraints.CENTER, CellConstraints.FILL));
         outputDirectoryLabelWizard4 = new JLabel();
         Font outputDirectoryLabelWizard4Font = this.$$$getFont$$$("Century Schoolbook L", -1, 18, outputDirectoryLabelWizard4.getFont());
         if (outputDirectoryLabelWizard4Font != null)
             outputDirectoryLabelWizard4.setFont(outputDirectoryLabelWizard4Font);
         outputDirectoryLabelWizard4.setForeground(new Color(-131585));
         outputDirectoryLabelWizard4.setText("<html><center>Select directory with generated <br>allotment files : <center><html>");
-        jPanelAnalyseWizard4.add(outputDirectoryLabelWizard4, cc.xy(1, 5, CellConstraints.RIGHT, CellConstraints.DEFAULT));
+        jPanelAnalyseWizard4.add(outputDirectoryLabelWizard4, cc.xy(1, 7, CellConstraints.RIGHT, CellConstraints.DEFAULT));
         outputDirectoryTextFieldWizard1 = new JTextField();
         outputDirectoryTextFieldWizard1.setEditable(false);
         outputDirectoryTextFieldWizard1.setEnabled(false);
@@ -208,7 +202,7 @@ public class analyzeAllotmentsWizard4 extends JFrame {
         if (outputDirectoryTextFieldWizard1Font != null)
             outputDirectoryTextFieldWizard1.setFont(outputDirectoryTextFieldWizard1Font);
         outputDirectoryTextFieldWizard1.setText("");
-        jPanelAnalyseWizard4.add(outputDirectoryTextFieldWizard1, cc.xy(3, 5, CellConstraints.FILL, CellConstraints.CENTER));
+        jPanelAnalyseWizard4.add(outputDirectoryTextFieldWizard1, cc.xy(3, 7, CellConstraints.FILL, CellConstraints.CENTER));
         outputDirectoryBrowseButtonWizard1 = new JButton();
         outputDirectoryBrowseButtonWizard1.setBorderPainted(false);
         outputDirectoryBrowseButtonWizard1.setEnabled(false);
@@ -217,28 +211,34 @@ public class analyzeAllotmentsWizard4 extends JFrame {
             outputDirectoryBrowseButtonWizard1.setFont(outputDirectoryBrowseButtonWizard1Font);
         outputDirectoryBrowseButtonWizard1.setIcon(new ImageIcon(getClass().getResource("/gui/dir1-small.png")));
         outputDirectoryBrowseButtonWizard1.setText("Browse");
-        jPanelAnalyseWizard4.add(outputDirectoryBrowseButtonWizard1, cc.xy(7, 5, CellConstraints.CENTER, CellConstraints.DEFAULT));
+        jPanelAnalyseWizard4.add(outputDirectoryBrowseButtonWizard1, cc.xy(7, 7, CellConstraints.CENTER, CellConstraints.DEFAULT));
         outputDirectoryCheckButtonWizard1 = new JButton();
         outputDirectoryCheckButtonWizard1.setBorderPainted(false);
         outputDirectoryCheckButtonWizard1.setContentAreaFilled(false);
         outputDirectoryCheckButtonWizard1.setText("");
-        jPanelAnalyseWizard4.add(outputDirectoryCheckButtonWizard1, cc.xy(5, 5, CellConstraints.CENTER, CellConstraints.DEFAULT));
+        jPanelAnalyseWizard4.add(outputDirectoryCheckButtonWizard1, cc.xy(5, 7, CellConstraints.CENTER, CellConstraints.DEFAULT));
         getAllotmentStatisticsButtonWizard4 = new JButton();
         getAllotmentStatisticsButtonWizard4.setBorderPainted(false);
         getAllotmentStatisticsButtonWizard4.setText("Get Chosen Allotment's Statistics");
-        jPanelAnalyseWizard4.add(getAllotmentStatisticsButtonWizard4, cc.xy(3, 7, CellConstraints.CENTER, CellConstraints.DEFAULT));
+        jPanelAnalyseWizard4.add(getAllotmentStatisticsButtonWizard4, cc.xy(3, 9, CellConstraints.CENTER, CellConstraints.DEFAULT));
         getStudentStatisticsButtonWizard4 = new JButton();
         getStudentStatisticsButtonWizard4.setBorderPainted(false);
         getStudentStatisticsButtonWizard4.setText("Get Student Specific Statistics");
-        jPanelAnalyseWizard4.add(getStudentStatisticsButtonWizard4, cc.xy(3, 9, CellConstraints.CENTER, CellConstraints.DEFAULT));
+        jPanelAnalyseWizard4.add(getStudentStatisticsButtonWizard4, cc.xy(3, 11, CellConstraints.CENTER, CellConstraints.DEFAULT));
         getCourseStatisticsButtonWizard4 = new JButton();
         getCourseStatisticsButtonWizard4.setBorderPainted(false);
         getCourseStatisticsButtonWizard4.setText("Get Course Specific Statistics");
-        jPanelAnalyseWizard4.add(getCourseStatisticsButtonWizard4, cc.xy(3, 11, CellConstraints.CENTER, CellConstraints.DEFAULT));
+        jPanelAnalyseWizard4.add(getCourseStatisticsButtonWizard4, cc.xy(3, 13, CellConstraints.CENTER, CellConstraints.DEFAULT));
         prevButtonWizard4 = new JButton();
         prevButtonWizard4.setBorderPainted(false);
         prevButtonWizard4.setText("Go Back");
-        jPanelAnalyseWizard4.add(prevButtonWizard4, cc.xy(3, 13, CellConstraints.CENTER, CellConstraints.DEFAULT));
+        jPanelAnalyseWizard4.add(prevButtonWizard4, cc.xy(3, 15, CellConstraints.CENTER, CellConstraints.DEFAULT));
+        titleFieldAnalysisWizard4 = new JLabel();
+        Font titleFieldAnalysisWizard4Font = this.$$$getFont$$$("Lobster Two", Font.BOLD, 60, titleFieldAnalysisWizard4.getFont());
+        if (titleFieldAnalysisWizard4Font != null) titleFieldAnalysisWizard4.setFont(titleFieldAnalysisWizard4Font);
+        titleFieldAnalysisWizard4.setForeground(new Color(-301758));
+        titleFieldAnalysisWizard4.setText("Welcome to SEAT");
+        jPanelAnalyseWizard4.add(titleFieldAnalysisWizard4, cc.xyw(2, 3, 3, CellConstraints.CENTER, CellConstraints.DEFAULT));
     }
 
     /**
